caa21d5a1250d9e8cd41e0d8fd83580508f48fc9
unknown <shivdattmasne2014@gamil.com>
2023-10-01 14:19:32 +0530

    Added arraylistdemo file

---
+++ CollectionDemo/src/admin/Array.java
@@ -0,0 +1,14 @@
+package admin;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class Array {
+
+	public static void main(String args[])
+	{
+		List<String> list = Arrays.asList("shiv","datt","vivek","ayash","riya");
+		
+		System.out.println(list);
+	}
+}
